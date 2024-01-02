36be0de5cb90ed0b44365065d9593b4f5ebb4514
xiangnan.liu <x22190562@student.ncirl.ie>
2024-01-02 02:20:50 +0000

    optimized the code

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,44 +1,20 @@
 $(document).ready(function () {
-    // Get weather infomation
+    // Get weather information
     weather();
 
     // click logout functionality
-    logoutBtn.click(function () {
+    $('#logout-btn').click(function () {
         window.location.href = window.location.origin + '/user/logout';
-        // Show login form again and hide user welcome information
-        /*loginContainer.show();
-        userWelcomeContainer.hide();*/
     });
 
     // Number of leaves to create
-    const numLeaves = 10;
-    for (let i = 0; i < numLeaves; i++) {
+    for (let i = 0; i < 10; i++) {
         createLeaf();
     }
 
 });
 
-
 const leavesContainer = $('.falling-leaves');
-const loginContainer = $('#login');
-const userWelcomeContainer = $('#user-welcome');
-const welcomeMessage = $('#welcome-message');
-const loginBtn = $('#login-btn');
-const logoutBtn = $('#logout-btn');
-
-/**
- * successful login triggers function
- * @param username
- */
-function onLoginSuccess(username) {
-    // Hide login form
-    loginContainer.hide();
-
-    // Show user welcome information
-    userWelcomeContainer.show();
-    welcomeMessage.text('Hello, ' + username);
-
-}
 
 /**
  * create leaves
@@ -53,6 +29,9 @@ function createLeaf() {
     leavesContainer.append(leaf);
 }
 
+/**
+ * Get weather information of current location
+ */
 function weather() {
     // Get current latitude and longitude
     if (navigator.geolocation) {
