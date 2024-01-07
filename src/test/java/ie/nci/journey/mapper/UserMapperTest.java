2ad2c18b95ac41e276cde8b8e5e083b796473057
xiangnan.liu <x22190562@student.ncirl.ie>
2024-01-07 00:26:27 +0000

    Delete the test method

--- src/test/java/ie/nci/journey/mapper/UserMapperTest.java
+++ src/test/java/ie/nci/journey/mapper/UserMapperTest.java
@@ -1,11 +1,8 @@
 package ie.nci.journey.mapper;
 
-import ie.nci.journey.entity.User;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Date;
 
 /**
  * UserMapperTest
@@ -19,10 +16,6 @@ public class UserMapperTest {
     private UserMapper userMapper;
 
     @Test
-    public void testInsertUser(){
-        User user = new User();
-        user.setPassword("321");
-        user.setCreateTime(new Date());
-        userMapper.insert(user);
+    public void testInsertUser() {
     }
 }
